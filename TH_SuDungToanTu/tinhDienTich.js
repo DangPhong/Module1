53cc95c583f193ebd46b8bd6d7acd0bfb36be8d6
DangPhong <dangphong.92.tt@gmail.com>
2019-05-27 17:56:24 +0700

    add TH_SuDungToanTu folder

---
+++ TH_SuDungToanTu/tinhDienTich.js
@@ -0,0 +1,7 @@
+var dai=parseInt(dai);;
+var rong=parseInt(rong);;
+dai=prompt('Nhap chiều dài: ')
+rong= prompt('Nhap chiều rộng: ');
+
+var dienTich=dai*rong;
+document.write('Diện tích HCN là: '+dienTich)
