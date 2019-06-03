76bac5e18617a0cab1b27253342b48e12086d2f0
DangPhong <dangphong.92.tt@gmail.com>
2019-06-03 17:23:18 +0700

    add file

---
+++ 4CauTrucLap/BT_HienThiCacSoNguyenToDauTien/isPrime.js
@@ -0,0 +1,28 @@
+function ketQuaNguyenTo(n) {
+    var ketQua = true;
+    if (n < 2)
+        ketQua = false;
+    else if (n == 2)
+        ketQua = true;
+    else if (n % 2 == 0) {
+        ketQua = false;
+    } else {
+        for (let i = 3; i < n - 1; i += 2) {
+            if (n % i == 0) {
+                ketQua = false;
+                break;
+            }
+        }
+    }
+    return ketQua;
+}
+var dem = 0;
+var i = 2;
+while (dem < 20) {
+    if (ketQuaNguyenTo(i)) {
+        dem++;
+        document.write(i + "<br>");
+    }
+    i++;
+
+}
