01e1d9d434abc7d5ce0ff834533423b4f01a5fce
DangPhong <dangphong.92.tt@gmail.com>
2019-06-27 01:15:44 +0700

    add file

--- CaseStudy/game.js
+++ CaseStudy/game.js
@@ -1,7 +1,12 @@
+var soundBacground = new Audio("audio/background.mp3")
+var soundGameOver = new Audio("audio/gameover.wav")
+var soundWall = new Audio("audio/wall.wav");
+var soundBrick = new Audio("audio/brick.wav");
+var soundWin = new Audio("audio/win.wav")
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 var ball = {
-    x: canvas.width / 2 ,
+    x: canvas.width / 2,
     y: canvas.height - 20,
     radius: 10,
     dx: 5,
@@ -17,43 +22,49 @@ var paddle = {
     isMoveingRight: false,
 }
 
+var brickConfig = {
+    offSetX: 0,
+    offSetY: 0,
+    margin: 5,
+    width: 70,
+    height: 15,
+    totalRow: 3,
+    totalCol: 7
+}
 var isGameOver = false;
-// var brickConfig = {
-//     offSetX: 25,
-//     offSetY:25,
-//     margin:25,
-//     width: 70,
-//     height:15,
-//     totalRow:3,
-//     totalCol:5
-// }
-// var brickList = [];
+var isGameWin = false;
+var userScore = 0;
+var maxScore = brickConfig.totalCol * brickConfig.totalRow;
 
-// for (let i = 0; i < brickList.totalRow; i++) {
-//     for (let j = 0; j < brickList.totalCol; j++) {
-//         brickList.push({
-//             x: brickConfig.offSetX + j * (brickConfig.width + brickConfig.margin),
-//             y: brickConfig.offSetY + i * (brickConfig.height + brickConfig.margin),
-//             isBroken: false
-//         });
 
-//     }
 
-// }
-// function drawBricks() {
-//     brickList.forEach(function (b) {
-//         context.beginPath();
-//         context.fillStyle = 'blue';
-//         context.rect(b.x, b.y, brickConfig.width, brickConfig.height);
-//         context.fill();
-//         context.closePath();
-//     });
-// }
+var brickList = [];
+for (let i = 0; i < brickConfig.totalRow; i++) {
+    for (let j = 0; j < brickConfig.totalCol; j++) {
+        brickList.push({
+            x: brickConfig.offSetX + j * (brickConfig.width + brickConfig.margin),
+            y: brickConfig.offSetY + i * (brickConfig.height + brickConfig.margin),
+            isBroken: false
+        });
+    }
+}
+// Vẽ những viên gạch
 
+function drawBricks() {
+    brickList.forEach(function (brick) {
+        if (!brick.isBroken) {
+            context.beginPath();
+            context.fillStyle = "#c63939";
+            context.rect(brick.x, brick.y, brickConfig.width, brickConfig.height);
+            context.fill();
+            context.closePath();
+        }
+    })
+}
 
 // nhả phím {37 trái - 39 phải}
 document.addEventListener('keyup', function (event) {
-    console.log('Key UP');
+    //console.log('Key UP');
     if (event.keyCode == 37) {
         paddle.isMovingLeft = false;
     } else if (event.keyCode == 39) {
@@ -63,7 +74,7 @@ document.addEventListener('keyup', function (event) {
 
 // nhấn phím
 document.addEventListener('keydown', function (event) {
-    console.log('Key DOWN');
+    //console.log('Key DOWN');
     if (event.keyCode == 37) {
         paddle.isMovingLeft = true;
     } else if (event.keyCode == 39) {
@@ -75,7 +86,7 @@ document.addEventListener('keydown', function (event) {
 function drawBall() {
     context.beginPath();
     context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    context.fillStyle = 'red';
+    context.fillStyle = '#4646b9';
     context.fill();
     context.closePath();
 }
@@ -84,41 +95,26 @@ function drawBall() {
 function drawPaddle() {
     context.beginPath();
     context.rect(paddle.x, paddle.y, paddle.width, paddle.height);
+    context.fillStyle = "#63b946";
     context.fill();
     context.closePath();
 }
 
-// Vẽ những viên gạch
-/*  
-    2 * OFFSET + 7*WiDTH + 6*Margin = 500
-    ROW =7 ; 
-    COL = 7
-
-*/
-function drawBricks() {
-    for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < 7; j++) {
-            context.beginPath();
-            context.rect(
-                0 + j * (70 + 2),
-                0 + i * (15 + 2),
-                70, 15
-            );
-            context.fill();
-            context.closePath();
-
-        }
-
-    }
-}
 // Xử lý bóng va chạm vs tường
 function handleBallCollideWall() {
+    //kiểm tra va chạm 2 biên
     if (ball.x < ball.radius || ball.x > canvas.width - ball.radius) {
         ball.dx = -ball.dx;
+        soundWall.load();
+        soundWall.play();
     }
+    //kiểm tra va chạm phía trên
     if (ball.y < ball.radius) {
         ball.dy = -ball.dy;
+        soundWall.load();
+        soundWall.play();
     }
+
 }
 
 // Xử lý bóng va chạm vs thanh chắn
@@ -127,6 +123,28 @@ function handleBallCollidePaddle() {
         ball.y + ball.radius >= canvas.height - paddle.height) {
         ball.dy = -ball.dy;
     }
+}
+
+// Xử lý bóng va chạm vs gạch
+function handleBallColideBricks() {
+    brickList.forEach(function (brick) {
+        if (!brick.isBroken) {
+            if (ball.x >= brick.x && ball.x <= brick.x + brickConfig.width &&
+                ball.y + ball.radius >= ball.y && ball.y - ball.radius <= brick.y + brickConfig.height) {
+                ball.dy = -ball.dy;
+                brick.isBroken = true;
+                userScore++;
+                console.log('score: ' + userScore);
+
+                if (userScore >= maxScore) {
+                    isGameOver = true;
+                    isGameWin = true;
+                }
+                soundBrick.load();
+                soundBrick.play();
+            }
+        }
+    })
 }
 
 // cập nhật vị trí quả bóng
@@ -156,23 +174,43 @@ function checkGameOver() {
     if (ball.y > canvas.height - ball.radius) {
         isGameOver = true;
     }
+
 }
 
 // xử lí kết thúc 
 function handleGameOver() {
-    console.log('Game Over!');
+
+    if (isGameWin) {
+        console.log("you win!");
+        soundWin.load();
+        soundWin.play();
+    } else {
+        console.log('Game Over!');
+        soundBacground.pause();
+        soundGameOver.play();
+    }
+
+}
+
+function playBackgroudSound() {
+    soundBacground.load();
+    soundBacground.volume = .5;
+    soundBacground.play();
 }
 
 
 function update() {
+
     if (!isGameOver) {
+        playBackgroudSound();
         context.clearRect(0, 0, canvas.clientWidth, canvas.height);
         drawBall();
         drawPaddle();
         drawBricks();
         updatePaddlePosition();
         handleBallCollidePaddle();
-        handleBallCollideWall()
+        handleBallCollideWall();
+        handleBallColideBricks();
         updateBallPosition();
         checkGameOver();
         requestAnimationFrame(update);
